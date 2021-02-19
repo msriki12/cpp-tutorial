4b1ded0b7dc86047b44b7780d640348f07f0b66b
Ricardo José <ricardomarsanc@gmail.com>
2021-02-19 13:34:57 +0100

    Added some tutorials

---
+++ FloatVars/main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<conio.h> // I/O management
+
+using namespace std;
+
+int main() {
+    float f = 2.567;
+    float f2 = 1.3;
+    cout << f + f2 << endl;
+    getch(); // Reads a char from the console without using the buffer and without showing it on the output
+    return 0;
+}

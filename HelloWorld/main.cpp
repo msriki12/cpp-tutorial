306b11a4f87b3fd1424e0a048851352a09174535
Ricardo José <ricardomarsanc@gmail.com>
2021-02-19 12:48:59 +0100

    First Commit

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h> // Muy parecido a C normal
+#include <stdlib.h>
+
+int main() {
+    printf("Hola Mundo.\n\n");
+    system("PAUSE"); // Continua ejecutando el programa hasta que se pulsa una tecla
+    return 0;
+}
